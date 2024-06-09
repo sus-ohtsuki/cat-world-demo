7b9160c9aa789a183f017a28e6889dcc68b3c239
sus.ohtsuki <s-ohtsuki@s-ucs.com>
2024-06-09 20:31:57 +0900

    feat: fade-in animation

--- assets/js/common.js
+++ assets/js/common.js
@@ -2,11 +2,33 @@
   document.addEventListener('DOMContentLoaded', function () {
     // Hamburger Menu
     hamburgerMenuToggle();
+
+    // Fade In
+    let elements = document.querySelectorAll('.fade-in');
+
+    function checkPosition() {
+      let windowHeight = window.innerHeight;
+      elements.forEach(function (element) {
+        let positionFromTop = element.getBoundingClientRect().top;
+
+        if (positionFromTop - windowHeight <= 0) {
+          element.classList.add('show');
+        } else {
+          element.classList.remove('show');
+        }
+      });
+    }
+
+    window.addEventListener('scroll', checkPosition);
+    window.addEventListener('resize', checkPosition);
+
+    // 初回ロード時にもチェック
+    checkPosition();
   });
 
   function hamburgerMenuToggle() {
-    var menuIcon = document.querySelector('.menu-icon-area');
-    var mobileMenuOverlay = document.getElementById('mobileMenuOverlay');
+    let menuIcon = document.querySelector('.menu-icon-area');
+    let mobileMenuOverlay = document.getElementById('mobileMenuOverlay');
 
     menuIcon.addEventListener('click', function () {
       mobileMenuOverlay.classList.add('active');
